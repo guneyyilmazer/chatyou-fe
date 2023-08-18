d7e65aca5eb614a939b0738b572d423c428667cc
Güney Yılmazer <guneyfewm@gmail.com>
2023-08-18 18:16:52 +0300

    added bootstrap to the project

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
- /*  <React.StrictMode> */ //to prevent double api calls
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element ={<App />}/>
-          
-        </Route>
-      </Routes>
-    </BrowserRouter>
-/*   </React.StrictMode> */
+  /*  <React.StrictMode> */ //to prevent double api calls
+  <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<App />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  /*   </React.StrictMode> */
 );
