4d33f303ed70acb496ba1f2fd4834e490393bce3
Güney Yılmazer <guneyfewm@gmail.com>
2023-08-19 12:29:48 +0300

    set up backend and socket.io

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+require("mongoose");
+const io = require("socket.io")(3001, {
+  maxHttpBufferSize: 1e7,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+const app = express();
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port " + process.env.PORT);
+});
+
+io.on("connection",(socket)=>{
+    console.log(socket.id+" connected")
+})
