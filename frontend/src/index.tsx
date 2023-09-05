2f2f8cc96c0e6964546e15bd40b82aacaeb18614
Güney Yılmazer <guneyfewm@gmail.com>
2023-09-06 00:21:49 +0300

    added profile pages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -20,6 +20,9 @@ root.render(
         <Route path="auth">
           <Route index element={<AuthPage />} />
         </Route>
+        <Route path="users">
+            <Route path=":username" element={<ProfilePage />} />
+          </Route>
       </Route>
     </Routes>
   </BrowserRouter>
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,12 +1,19 @@
-import React, { useState, useMemo, useEffect } from "react";
-import NotFound404 from "../pages/NotFound404.jsx";
+import { useState, useMemo } from "react";
+import NotFound404 from "../pages/NotFound404";
 import { useParams } from "react-router-dom";
 import Cookies from "js-cookie";
 const ProfilePage = () => {
   const { username } = useParams();
-
-  const [user, setUser] = useState({});
-  const [client, setClient] = useState({});
+type user = {
+    username:string,
+    profilePicture:string,
+    id:string
+}
+type client= {
+    userId:string
+}
+  const [user, setUser] = useState<user>();
+  const [client, setClient] = useState<client>();
   const getUserData = async () => {
     const res = await fetch("http://localhost:4000/user/loadUser", {
       headers: {
@@ -30,13 +37,41 @@ const ProfilePage = () => {
       method: "POST",
       body: JSON.stringify({ token: Cookies.get("Auth_Token") }),
     });
-    const client = response.json();
+    const client = await response.json();
     setClient(client);
   };
 
   useMemo(getUserData, []);
 
-  return <div className="text-white"></div>;
+  return <div className="text-white d-flex flex-column justify-content-center align-items-center">
+  {(user && client) &&<>
+  <img src={user.profilePicture} style={{ width: "30vw" }} />
+    {user.username ? (
+    <div className="lead">{user.username}</div>
+  ) : (
+    <NotFound404 />
+  )}
+
+  {user.id &&
+    (user.id == client.userId ? (
+      <span>MyProfilePage</span>
+    ) : (
+      <button
+      className="btn btn-dark"
+        onClick={() => {
+          localStorage.setItem("chattingWith", user.username);
+          localStorage.removeItem("room")
+          window.location.replace("/")
+        }}
+      >
+        Send a message
+      </button>
+    ))}
+    
+    
+    </>}
+  
+</div>
 };
 
 export default ProfilePage;
