6598065192f8344e72fbc3ad257e2d4bcdc735b5
Güney Yılmazer <guneyfewm@gmail.com>
2023-09-16 22:27:27 +0300

    added seenBy property to the roomSchema

--- backend/schemas/roomSchema.js
+++ backend/schemas/roomSchema.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 const roomSchema = mongoose.Schema({
   name: { required: true, type: String },
-  privateRoom:{type:String,default:false},
+  privateRoom: { type: String, default: false },
   messages: [
     {
-      sender: { 
-        username:{type:String,required:true},
-        userId:{type:String,required:true}
-       },
+      sender: {
+        username: { type: String, required: true },
+        userId: { type: String, required: true },
+      },
+      seenBy: [{ userId: { type: String, required: true } }],
       content: { required: true, type: String },
       pictures: [String],
-      sent: {type:Date,default:Date.now()},
-      seen:{type:Boolean,default:false}
+      sent: { type: Date, default: Date.now() },
+      seen: { type: Boolean, default: false },
     },
   ],
 });
