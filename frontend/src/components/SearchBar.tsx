0c0b516de9f13bfdfa1bc9932d7fcc636ffda459
Güney Yılmazer <guneyfewm@gmail.com>
2023-09-21 22:42:45 +0300

    styled SearchBarResults.tsx and made it render users

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,12 +1,14 @@
 import Cookies from "js-cookie";
 import { useRef, useState } from "react";
+import SearchBarResults from "./SearchBarResults";
+import { setUser } from "../features/appSlice";
 
 const SearchBar = () => {
   const [searchFor, setSearchFor] = useState("users");
   const usernameInputRef = useRef<HTMLInputElement>(null);
   const roomInputRef = useRef<HTMLInputElement>(null);
-  const [results, setResults] = useState([]);
-  console.log(results)
+  const [rooms, setRooms] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const findUsers = async () => {
     const res = await fetch("http://localhost:4000/user/findUsers", {
@@ -23,7 +25,7 @@ const SearchBar = () => {
     const response = await res.json();
     console.log(response);
     if (!response.error) {
-      setResults(response);
+      setUsers(response.users);
     }
   };
   const findRoom = async () => {
@@ -41,10 +43,13 @@ const SearchBar = () => {
     const response = await res.json();
     console.log(response);
     if (!response.error) {
-      setResults(response);
+      setRooms(response.rooms);
     }
   };
   return (
+    <div>
+
+
     <form className="form-group d-flex">
       <input
         ref={searchFor == "users" ? usernameInputRef : roomInputRef}
@@ -52,9 +57,11 @@ const SearchBar = () => {
         type="text"
         className="form-check p-1 text-center"
         placeholder="Search"
-      />
+        />
       Search
     </form>
+    <SearchBarResults users={users} rooms={rooms}/>
+        </div>
   );
 };
 
--- frontend/src/components/SearchBarResults.tsx
+++ frontend/src/components/SearchBarResults.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import '../css/SearchBar.css'
+const DefaultProfilePicture = require("../images/default.jpeg")
 
-const SearchBarResults = () => {
+const SearchBarResults = ({ users, rooms }: any) => {
   return (
-    <div>SearchBarResults</div>
-  )
-}
+    <div className="bg-dark">
+      {users &&
+        users.map((item: any) => (
+          <div className="text-white results d-flex py-3 ps-2 align-items-center">
+            
+            <img
+              style={{ height: "40px", width: "40px" }}
+              className="rounded-3"
+              src={item.profilePicture ? item.profilePicture : DefaultProfilePicture}
+            />
+            <span className="ms-2">
+            {item.username}
+                </span>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-export default SearchBarResults
+export default SearchBarResults;
