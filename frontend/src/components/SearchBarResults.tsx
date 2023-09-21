cac0c8c7bacb5a322c627ce859d31cebb1b686df
Güney Yılmazer <guneyfewm@gmail.com>
2023-09-21 23:32:03 +0300

    made users in SearcBarResults.tsx clickable
    It directs you to the profile page of the user that you click on

--- frontend/src/components/SearchBarResults.tsx
+++ frontend/src/components/SearchBarResults.tsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
-import '../css/SearchBar.css'
-const DefaultProfilePicture = require("../images/default.jpeg")
+import "../css/SearchBar.css";
+import { Link } from "react-router-dom";
+const DefaultProfilePicture = require("../images/default.jpeg");
 
 const SearchBarResults = ({ users, rooms }: any) => {
   return (
     <div className="bg-dark">
       {users &&
         users.map((item: any) => (
-          <div className="text-white results d-flex py-3 ps-2 align-items-center">
-            
-            <img
-              style={{ height: "40px", width: "40px" }}
-              className="rounded-3"
-              src={item.profilePicture ? item.profilePicture : DefaultProfilePicture}
-            />
-            <span className="ms-2">
-            {item.username}
-                </span>
+          <div className="results d-flex py-3 ps-2 align-items-center">
+            <Link to={`users/${item._id}`}>
+              <img
+                style={{ height: "40px", width: "40px" }}
+                className="rounded-3"
+                src={
+                  item.profilePicture
+                    ? item.profilePicture
+                    : DefaultProfilePicture
+                }
+              />
+              <span className="ms-2 text-white text-decoration-none">
+                {item.username}
+              </span>
+            </Link>
           </div>
         ))}
     </div>
